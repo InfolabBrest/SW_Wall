97aa896e8caeee3a84d23c01c5f13e0fcaf4b254
Pierre Zemb <pierre.zemb.isen@gmail.com>
2015-01-26 16:20:23 +0100

    adding beginning of UI for punching ball wait 10sec to view

--- public/ngcontrollers.js
+++ public/ngcontrollers.js
@@ -2,16 +2,24 @@ var myApp = angular.module('SWApp', []);
 myApp.controller('punchingBallTop10Controller', function ($scope, $http) {
 
 	$scope.top_10 = new Array();
+	$scope.top_3 = new Array();
 	// fetching top 10 every 10 seconds
-	setInterval(function () {
+
+
+	$scope.refresh_top = function (){
 		$http.get('/punchingball/top10').
 		  success(function(data, status, headers, config) {
-		  	$scope.top_10 = data;
+		  	$scope.top_3 = data.slice(0,3);
+		  	$scope.top_10 = data.slice(3,11);
+
+		  	console.log($scope.top_3);
+		  	console.log($scope.top_10);
 		  }).
 		  error(function(data, status, headers, config) {
 		    // called asynchronously if an error occurs
 		    // or server returns response with an error status.
 		  });
-
-	}, 10*1000);
+	};
+	$scope.refresh_top;
+	setInterval($scope.refresh_top, 10*1000);
 });
